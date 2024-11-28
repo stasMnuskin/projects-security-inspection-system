efdaa7f4f8f898c58c4bfad1bf4c60b4333db5f3
Stas Mnuskin <87757002+stasMnuskin@users.noreply.github.com>
2024-11-28 09:58:14 +0200

    Update database.js

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -25,7 +25,7 @@ module.exports = {
     logging: console.log
   },
   production: {
-    username: 'securityapp',
+    username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
